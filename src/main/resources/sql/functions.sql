3456d3cb12c18e8e9d15e2544d1b028d684b0863
RomanQed <gbakaldin@gmail.com>
2024-05-30 16:51:08 +0300

    Clean up sql code for functions

--- src/main/resources/sql/functions.sql
+++ src/main/resources/sql/functions.sql
@@ -1,17 +1,15 @@
-create
-or replace function check_table_exists(in tbl text)
+create function check_table_exists(in tbl text)
 returns bool as $$
 begin
-return (select exists (select * from information_schema.tables where table_name = tbl and table_schema = 'public'));
+    return (select exists (select * from information_schema.tables where table_name = tbl and table_schema = 'public'));
 end;
 $$
 language plpgsql;
 
-create
-or replace function check_tables_exist(in tbls text[])
+create function check_tables_exist(in tbls text[])
 returns bool as $$
 declare
-e text;
+    e text;
 begin
 	foreach e in array tbls
 	loop
@@ -19,7 +17,7 @@ begin
 			return false;
         end if;
     end loop;
-return true;
+    return true;
 end;
 $$
 language plpgsql;
